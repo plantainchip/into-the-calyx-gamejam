218fad3074b1c79ae03d0bb2bde3ac93fcb0a3c1
plantainchip <maitrivathada@gmail.com>
2025-08-17 15:26:57 -0400

    changed ending imports

--- src/ending.js
+++ src/ending.js
@@ -1,7 +1,13 @@
 import "kaplay/global";
+import game_ending_png from "/sprites/assets/animations/ending_cutscene.png";
+import game_ending_json from "/sprites/assets/animations/ending_cutscene.json?url";
 
 export default function (STATE) {
-    loadAseprite("ending_cutscene", "./sprites/assets/animations/ending_cutscene.png", "./sprites/assets/animations/ending_cutscene.json");
+    loadAseprite("ending_cutscene", game_ending_png, game_ending_json);
+
+    loadAseprite("ending_cutscene_part1", "./sprites/assets/animations/ending_cutscene_part1.png", "./sprites/assets/animations/ending_cutscene_part1.json");
+    loadAseprite("ending_cutscene_part2", "./sprites/assets/animations/ending_cutscene_part2.png", "./sprites/assets/animations/ending_cutscene_part2.json");
+    loadAseprite("ending_cutscene_part3", "./sprites/assets/animations/ending_cutscene_part3.png", "./sprites/assets/animations/ending_cutscene_part3.json");
 
     const cutscene = add([
         sprite("ending_cutscene", {
@@ -18,6 +24,34 @@ export default function (STATE) {
         //     go("backyard", STATE);
         // }
     });
+
+    // const cutscene1 = add([
+    //     sprite("ending_cutscene_part1"),
+    //     pos(0, 0),
+    // ])
+    // const cutscene2 = add([
+    //     sprite("ending_cutscene_part2"),
+    //     pos(0, 0),
+    // ])
+    // const cutscene3 = add([
+    //     sprite("ending_cutscene_part3"),
+    //     pos(0, 0),
+    // ])
+    // cutscene1.play("ed1", {
+    //     loop: false,
+    //     onEnd: () => {
+    //         // go to backyard scene after cutscene ends
+    //         cutscene2.play("ed2", {
+    //             loop: false,
+    //             onEnd: () => {
+    //                 cutscene3.play("ed3", {
+    //                     loop: false
+    //                 })
+    //             }
+    //         })
+    //     }
+    // });
+
 
 
 }
